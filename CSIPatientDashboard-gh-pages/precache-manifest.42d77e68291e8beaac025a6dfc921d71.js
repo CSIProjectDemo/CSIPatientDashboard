3977fd09113315b7840ad853f3e2050ab9a5bd5c
CSIProjectDemo <85685237+CSIProjectDemo@users.noreply.github.com>
2021-06-10 09:39:31 -0400

    Add files via upload

---
+++ CSIPatientDashboard-gh-pages/precache-manifest.42d77e68291e8beaac025a6dfc921d71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac7253a27c87498bc6aacf3316545f0",
+    "url": "/CSIPatientDashboard/index.html"
+  },
+  {
+    "revision": "d1d0e600e3e1b39d6148",
+    "url": "/CSIPatientDashboard/static/css/2.054cb986.chunk.css"
+  },
+  {
+    "revision": "e1955e2c5e81bc068387",
+    "url": "/CSIPatientDashboard/static/css/main.57cff4e0.chunk.css"
+  },
+  {
+    "revision": "d1d0e600e3e1b39d6148",
+    "url": "/CSIPatientDashboard/static/js/2.33ef5b94.chunk.js"
+  },
+  {
+    "revision": "6013c9146057725ae63874396f4172e3",
+    "url": "/CSIPatientDashboard/static/js/2.33ef5b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1955e2c5e81bc068387",
+    "url": "/CSIPatientDashboard/static/js/main.4fbb5d0a.chunk.js"
+  },
+  {
+    "revision": "4b3cf5437d58d17adca2",
+    "url": "/CSIPatientDashboard/static/js/runtime-main.32b9f18f.js"
+  }
+]);
