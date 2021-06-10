badb0f8ecdbb149949f6edcf2dffaaab10a4aedd
CSIProjectDemo <citizenscienceinc@gmail.com>
2021-06-10 09:18:06 -0400

    Updates

---
+++ precache-manifest.44fa9c0973ecc6a99e839d799845eaa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c59943b98acbf84fbde20628081c218",
+    "url": "/CSIPatientDashboard/index.html"
+  },
+  {
+    "revision": "27a1db33108391c1041b",
+    "url": "/CSIPatientDashboard/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8aa651d6ff114993850e",
+    "url": "/CSIPatientDashboard/static/css/main.4899eac2.chunk.css"
+  },
+  {
+    "revision": "27a1db33108391c1041b",
+    "url": "/CSIPatientDashboard/static/js/2.658cb910.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/CSIPatientDashboard/static/js/2.658cb910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa651d6ff114993850e",
+    "url": "/CSIPatientDashboard/static/js/main.9921aa05.chunk.js"
+  },
+  {
+    "revision": "bbaec832c832a92b8aff",
+    "url": "/CSIPatientDashboard/static/js/runtime-main.74fe5c6a.js"
+  }
+]);
